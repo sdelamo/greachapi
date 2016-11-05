ce849f929aa5d8557964f07652a0a7d0d0f0bd4b
sdelamo <sergio.delamo@softamo.com>
2016-11-05 17:31:36 +0100

    Fetch email, youtube and address

--- scraper/src/main/groovy/com/groovycalamari/greachapi/geb/HomePage.groovy
+++ scraper/src/main/groovy/com/groovycalamari/greachapi/geb/HomePage.groovy
@@ -7,14 +7,50 @@ class HomePage extends Page {
     static url = '/'
 
     static content = {
-        crewLinks(required: false) { $('footer a') }
+        footerLinks(required: false) { $('footer a') }
+        addressDiv(required: false) { $('div.credits', 0) }
     }
 
     Set<CrewMember> crewMembers() {
-        if ( crewLinks.empty ) {
+        if ( footerLinks.empty ) {
             return new HashSet<CrewMember>()
         }
-        def twitterLinks = crewLinks.findAll { it.getAttribute('href').contains('twitter.com') }
+        def twitterLinks = footerLinks.findAll { it.getAttribute('href').contains('twitter.com') }
         twitterLinks.collect { new CrewMember(name: it.text(), twitter: it.getAttribute('href')) } as Set<CrewMember>
+    }
+
+    String address() {
+
+        if ( addressDiv.empty ) {
+            return ''
+        }
+        String text = addressDiv.text()
+        removeAdvertisingCopy(text)
+    }
+
+    String youtube() {
+        if ( footerLinks.empty ) {
+            return ''
+        }
+        footerLinks.find { it.getAttribute('href').contains('youtube') }?.getAttribute('href')
+    }
+
+    String email() {
+        if ( footerLinks.empty ) {
+            return ''
+        }
+        String href = footerLinks.find { it.getAttribute('href').contains('mailto:') }?.getAttribute('href')
+        if ( href ) {
+            return href.substring('mailto:'.length(), href.length())
+        }
+        href
+    }
+
+    static String removeAdvertisingCopy(String text) {
+        final String advertisingCopy = 'Powered by'
+        if ( text && text.indexOf(advertisingCopy) != -1 ) {
+            text = text.substring(0, text.indexOf(advertisingCopy))
+        }
+        text?.trim()
     }
 }
---
+++ scraper/src/test/groovy/com/groovycalamari/greachapi/AboutFetcherSpec.groovy
@@ -0,0 +1,31 @@
+package com.groovycalamari.greachapi
+
+import com.groovycalamari.greachapi.geb.HomePage
+import geb.Browser
+import spock.lang.Specification
+
+class AboutFetcherSpec extends Specification {
+
+    def "test address is fetched correctly"() {
+        when:
+        def browser = new Browser()
+        HomePage page = browser.to HomePage
+        String address = page.address()
+
+        then:
+        address == 'The Greach Network SL, 2011-2017 - CIF B86412491 - C/Valtravieso, 28023 Madrid (Spain)'
+
+        when:
+        def email = page.email()
+
+        then:
+        email == 'crew@greachconf.com'
+
+        when:
+        def youtube = page.youtube()
+
+        then:
+        youtube == 'http://www.youtube.com/user/TheGreachChannel'
+    }
+
+}
